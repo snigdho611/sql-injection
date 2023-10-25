b12e97e5fe1ca9db35e5559ad1acc66c7cfb5532
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-10-25 23:27:14 +0600

    Adding code for final update

--- controller/UserController.php
+++ controller/UserController.php
@@ -3,12 +3,10 @@ class UserController
 {
     function Login($conObj, $email, $password)
     {
-        try {
-            $SQL_QUERY = 'SELECT * FROM users WHERE email = \'' . $email . '\' AND password = \'' . $password . '\';';
-            $result = $conObj->query($SQL_QUERY);
+        $SQL_QUERY = 'SELECT * FROM users WHERE email = \'' . $email . '\' AND password = \'' . $password . '\';';
+        if ($result = $conObj->query($SQL_QUERY)) {
             return $result;
-        } catch (exception $e) {
-            print_r($e);
+        } else {
             return ["error" => "Internal server error"];
         }
     }
--- routes/auth/login.php
+++ routes/auth/login.php
@@ -1,23 +1,35 @@
 <?php
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+// header('Access-Control-Allow-Methods:  POST, PUT, GET');
 session_start();
 include("../../config/db.php");
-include("../../model/user.php");
 include("../../controller/UserController.php");
 
 $entityBody = json_decode(file_get_contents('php://input'));
 
 if ($entityBody->email && $entityBody->password) {
-    $databaseObj = new Database();
-    $conObj = $databaseObj->connect();
-    $User = new UserController();
-    $result = $User->Login($conObj, $entityBody->email, $entityBody->password);
-    header('Content-Type: application/json; charset=utf-8');
-    if (mysqli_num_rows($result) == 0) {
-        http_response_code(404);
-        print_r(json_encode(["error" => "Invalid credentials"]));
-    } else {
-        http_response_code(200);
-        print_r(json_encode($result->fetch_assoc()));
+    try {
+        $databaseObj = new Database();
+        $conObj = $databaseObj->connect();
+        $User = new UserController();
+        $result = $User->Login($conObj, $entityBody->email, $entityBody->password);
+        header('Content-Type: application/json; charset=utf-8');
+        if (mysqli_num_rows($result) == 0) {
+            http_response_code(404);
+            print_r(json_encode(["error" => "Invalid credentials"]));
+        } else {
+            http_response_code(200);
+            print_r(json_encode($result->fetch_assoc()));
+        }
+    }
+    // catch(mysqli_sql_exception $e){
+    //     return print_r(json_encode(["error" => "Internal server error", "source"=> "$e" ]));
+    // }
+    catch (exception $e) {
+        // print_r($e);
+        http_response_code(500);
+        return print_r(json_encode(["error" => "$e"]));
     }
 } else {
     header('Content-Type: application/json; charset=utf-8');
